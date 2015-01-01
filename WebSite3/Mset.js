a4343d7e743fccba6da758ef9c753bf2f26471eb
braunchr <christophebraun@hotmail.com>
2015-01-01 15:04:02 +0000

    first attempt at threading

--- WebSite3/Mset.js
+++ WebSite3/Mset.js
@@ -29,10 +29,13 @@ function MSet(maxiter, threshold)
         imgbit.fillDisk(i, j, radius, 50, 50, 200); //draw a circle and fill it. 
         imgbit.drawCircle(i, j, radius, 100, 100, 100);
         }
-    else if (dist > 1 / imgbit.pixOverX / this.threshold)
+    else if (dist > 1 / imgbit.pixOverX / this.threshold && dist < 4 / imgbit.pixOverX / this.threshold)
         {
-            imgbit.setPixel(i, j, 0, 0, 255); // the point is just outside the set
-        }
+            imgbit.setPixel(i, j, 200, 200, 200); // the point is just outside the set
+    }
+    else if (dist > 1 / imgbit.pixOverX / this.threshold && dist >= 4 / imgbit.pixOverX / this.threshold) {
+        imgbit.setPixel(i, j, 0, 0, 255); // the point is just outside the set
+    }
     else
         {
         imgbit.setPixel(i, j, 0, 0, 0); // the point is IN the set
---
+++ WebSite3/MsetWorker.js
@@ -0,0 +1,106 @@
+//importScripts("Mset.js");
+
+onmessage = function (e) {
+
+    //get the data from the event
+    var m = e.data.MsetName;
+    var imgbit = e.data.imgbitName;
+
+
+    //call the long computation
+    m.paint(imgbit);
+
+    //post back imagebit to the UI thread
+
+
+}
+
+
+
+
+function MSet(maxiter, threshold)
+{
+    this.maxiter = maxiter;
+    this.threshold = threshold;
+    this.xorbit = new Array(10000);/* Stores the orbit of x 	*/
+    this.yorbit = new Array(10000);/* Stores the orbit of y 	*/
+}
+
+ MSet.prototype.paintDisk = function( i,  j, imgbit)  
+{
+    var x = imgbit.x1 + i / imgbit.pixOverX;
+    var y = imgbit.y1 - j / imgbit.pixOverX;
+
+    var dist = 0.25 * this.distance(x, y);
+    var radius = Math.floor(dist * imgbit.pixOverX);
+
+    if (radius > 1)
+    {
+        imgbit.fillDisk(i, j, radius, 50, 50, 200); //draw a circle and fill it. 
+        imgbit.drawCircle(i, j, radius, 100, 100, 100);
+        }
+    else if (dist > 1 / imgbit.pixOverX / this.threshold && dist < 4 / imgbit.pixOverX / this.threshold)
+        {
+            imgbit.setPixel(i, j, 200, 200, 200); // the point is just outside the set
+    }
+    else if (dist > 1 / imgbit.pixOverX / this.threshold && dist >= 4 / imgbit.pixOverX / this.threshold) {
+        imgbit.setPixel(i, j, 0, 0, 255); // the point is just outside the set
+    }
+    else
+        {
+        imgbit.setPixel(i, j, 0, 0, 0); // the point is IN the set
+    }
+}
+
+MSet.prototype.distance = function(cx, cy)
+{
+    var x = 0, y = 0, x2 = 0, y2 = 0, dist = 0, iter = 0, temp = 0, xdc = 0, ydc = 0, i = 0, flag = false;
+    this.xorbit[0] = this.yorbit[0] = 0;
+  
+    while ((iter < this.maxiter) && ((x2 + y2) < 10000))
+    {
+        temp = x2 - y2 + cx;
+        y = 2 * x * y + cy;
+        x = temp;
+        x2 = x * x;
+        y2 = y * y;
+        iter++;
+        this.xorbit[iter] = x;
+        this.yorbit[iter] = y;
+    }
+
+    if ((x2 + y2) > 10000)
+    {
+        xdc = ydc = 0;
+        i = 0;
+        flag = false;
+        while ((i < iter) && (flag == false))
+        {
+            temp = 2 * (this.xorbit[i] * xdc - this.yorbit[i] * ydc) + 1;
+            ydc = 2 * (this.yorbit[i] * xdc + this.xorbit[i] * ydc);
+            xdc = temp;
+            flag = (Math.max(Math.abs(xdc), Math.abs(ydc)) > 1e300);
+            i++;
+        }
+        if (flag == false)
+            dist = Math.log(x2 + y2) * Math.sqrt(x2 + y2) / Math.sqrt(xdc * xdc + ydc * ydc);
+    }
+    return dist;
+}
+
+MSet.prototype.paint = function (imgbit) {
+    
+    for (var j = 0; j < imgbit.height; j++)
+    {
+        for (var i = 0; i<imgbit.width; i++)
+        {
+            if (imgbit.imgData.data[4 * (j*imgbit.width + i)+3] == 0) //only paint if the pixel is transparent.
+            {
+                this.paintDisk(i, j, imgbit);
+            }
+        }
+        
+    }
+    return imgbit;
+}
+
