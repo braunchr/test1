ce87e9de3a1f1e79a5bbcb1d958a28e8964c93f4
braunchr <christophebraun@hotmail.com>
2015-01-04 23:38:56 +0000

    ImgBit called from the worker

--- WebSite3/MsetWorker.js
+++ WebSite3/MsetWorker.js
@@ -1,12 +1,9 @@
 importScripts("Mset.js");
 importScripts("ImageBit.js");
 
-var oo;
-
 self.onmessage = function (e) {
 
     //get the data from the event
-    var imgbit = new ImageBit(e.data.imgbit.imgData, e.data.imgbit.width, e.data.imgbit.height, e.data.imgbit.x1, e.data.imgbit.y1, e.data.imgbit.x2);
     var threshold = e.data.threshold;
     var maxiter = e.data.maxiter;
     var px1 = e.data.px1;
@@ -14,8 +11,14 @@ self.onmessage = function (e) {
     var col1 = e.data.col1;
     var col2 = e.data.col2;
     var col3 = e.data.col3;
+    var imgData = e.data.imgData;
+    var subwidth=e.data.subwidth;
+    var subheight = e.data.subheight;
+    var subx1 = e.data.subx1;
+    var subx2 = e.data.subx2;
+    var suby1 = e.data.suby1;
 
-    oo=e.data.workerID;
+    var imgbit = new ImageBit(imgData, subwidth, subheight, subx1, suby1, subx2);
 
     //call the long computation
     var m = new MSet(maxiter, threshold, col1, col2, col3);
@@ -23,7 +26,7 @@ self.onmessage = function (e) {
 
     //formats the data to be returned back to the UI thread
     var workerMessage = {
-        imgbit: imgbit,
+        imgData: imgbit.imgData,
         px1: Math.round(px1),
         py1: Math.round(py1),
     };
